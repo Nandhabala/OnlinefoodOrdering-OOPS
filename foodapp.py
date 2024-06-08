bc531dc4709e89935cd27fb6d57801ca2a674f1f
Nandhabala <nandhapadmanathan1705@gmail.com>
2024-06-08 13:38:06 +0530

    changes

---
+++ foodapp.py
@@ -0,0 +1,5 @@
+class Foodapp:
+
+    @staticmethod
+    def init(): #intial method
+        print("<<<<<<<< welcome to online food ordering>>>>>>>")
---
+++ main.py
@@ -0,0 +1,4 @@
+#online food orderinds system
+
+from foodapp import Foodapp    #from filename import class name
+Foodapp.init()
